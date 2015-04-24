b386735252e7c6715150774703af70cfcd606f2e
JordanCollier <jordanicollier@gmail.com>
2015-04-24 11:24:16 -0600

    add module pattern

--- main.js
+++ main.js
@@ -1,62 +1,71 @@
+// ---------------ORIGINAL---------------
+// $(function(){
+//   var source = $("#rails_api").html();
+//   var template = Handlebars.compile(source);
 //
-// var products = {
-//     template: function() {
-//       return $("#products-template").html()
-//     },
-//     compile: function() {
-//       Handlebars.compile(this.template());
-//     },
-//     request: function() {
-//       var getProducts = $.getJSON("http://localhost:3001/api/products");
-//       getProducts.done(this.buildTemplate);
-//     },
-//     buildTemplate: function(data) {
-//       var html = compile(data);
-//       $('body').append(html);
-//     }
-//   }
 //
+//   $.ajax({
+//     url: "http://localhost:3000/api/products"
+//   }).done(function(data) {
+//
+//     var products = data;
+//     var html    = template(products);
+//     $('body').append(html);
+//   });
+//
+//
+// });
 
+// ------------MODULE PATTERN------------
+$( document ).ready(function() {
+  var feature = (function() {
+    var result = {
+      ajaxin:ajaxin
+    };
 
-var products = {
-  template: function() {
-    return $("#rails_api").html();
-  },
-  compile: function() {
+    var source = $("#rails_api").html();
+    var template = Handlebars.compile(source);
 
-    return Handlebars.compile(this.template());
-  },
-  request: function() {
-    var getProducts = $.ajax("http://localhost:3000/api/products");
-    getProducts.done(this.buildTemplate);
-  },
-  buildTemplate: function(data) {
-    var html = products.compile()(data);
-    console.log('data');
-    $('body').append(html);
-  },
-  init: function(){
-    this.request();
-  }
-}
-
-$(function(){
-  products.init();
+    function ajaxin() {
+      $.ajax({
+        url: "http://localhost:3000/api/products"
+      }).done(function(data) {
+        var products = data;
+        var html    = template(products);
+        $('body').append(html);
+      })
+    };
+    return result;
+  })();
+  feature.ajaxin();
 });
 
-  // $(function(){
-  //   var source = $("#rails_api").html();
-  //   var template = Handlebars.compile(source);
-  //
-  //
-  //   $.ajax({
-  //     url: "http://localhost:3000/api/products"
-  //   }).done(function(data) {
-  //
-  //     var products = data;
-  //     var html    = template(products);
-  //     $('body').append(html);
-  //   });
-  //
-  //
-  // });
+
+
+
+// ------------OBJECT PATTERN------------
+// var products = {
+//   template: function() {
+//     return $("#rails_api").html();
+//   },
+//   compile: function() {
+//
+//     return Handlebars.compile(this.template());
+//   },
+//   request: function() {
+//     var getProducts = $.ajax("http://localhost:3000/api/products");
+//     getProducts.done(this.buildTemplate);
+//   },
+//   buildTemplate: function(data) {
+//     var html = products.compile()(data);
+//     console.log('data');
+//     $('body').append(html);
+//   },
+//   init: function(){
+//     this.request();
+//   }
+// }
+//
+// $(function(){
+//   products.init();
+// });
